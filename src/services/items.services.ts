cc585d8018852469e1fb94f77158c6aff0be7753
Gilles Figuière <me@gillesfiguiere.com>
2018-09-17 22:03:57 +0200

    Added isLent attribute to item services

--- src/services/items.services.ts
+++ src/services/items.services.ts
@@ -4,37 +4,43 @@ export class ItemServices {
             title: "The Time Machine",
             author: "H. G. Wells",
             publishDate: "1995-04-03",
-            isbn: "0486284727"
+            isbn: "0486284727",
+            isLent: false
         },
         {
             title: "Do Androids Dream of Electric Sheep?",
             author: "Philip K. Dick",
             publishDate: "1996-05-28",
-            isbn: "5533117542"
+            isbn: "5533117542",
+            isLent: false
         },
         {
             title: "de la Terre À La Lune",
             author: "Jules Verne",
             publishDate: "1976-03-01",
-            isbn: "2253006319"
+            isbn: "2253006319",
+            isLent: false
         }
     ];
-    
+
     cdsList = [
         {
             title: "Showbiz",
             author: "Muse",
-            publishDate: "1999"
+            publishDate: "1999",
+            isLent: false
         },
         {
             title: "Origin of Symmetry",
             author: "Muse",
-            publishDate: "2001"
+            publishDate: "2001",
+            isLent: false
         },
         {
             title: "The 2nd Law",
             author: "Muse",
-            publishDate: "2012"
+            publishDate: "2012",
+            isLent: false
         }
     ];
 }

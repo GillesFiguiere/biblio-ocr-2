1414dbb98e07b6c3ae4b9e605cbee14d2fc889d9
Gilles Figuière <me@gillesfiguiere.com>
2018-09-17 21:59:55 +0200

    Added items services

---
+++ src/services/items.services.ts
@@ -0,0 +1,40 @@
+export class ItemServices {
+    booksList = [
+        {
+            title: "The Time Machine",
+            author: "H. G. Wells",
+            publishDate: "1995-04-03",
+            isbn: "0486284727"
+        },
+        {
+            title: "Do Androids Dream of Electric Sheep?",
+            author: "Philip K. Dick",
+            publishDate: "1996-05-28",
+            isbn: "5533117542"
+        },
+        {
+            title: "de la Terre À La Lune",
+            author: "Jules Verne",
+            publishDate: "1976-03-01",
+            isbn: "2253006319"
+        }
+    ];
+    
+    cdsList = [
+        {
+            title: "Showbiz",
+            author: "Muse",
+            publishDate: "1999"
+        },
+        {
+            title: "Origin of Symmetry",
+            author: "Muse",
+            publishDate: "2001"
+        },
+        {
+            title: "The 2nd Law",
+            author: "Muse",
+            publishDate: "2012"
+        }
+    ];
+}
